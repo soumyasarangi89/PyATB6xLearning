7804f7fc0f24f8d1cd93f28fb2431d2a6c9dfa1b
soumyasarangi89 <112792910+soumyasarangi89@users.noreply.github.com>
2025-10-14 23:17:53 +0530

    14th Oct 2025 3

--- src/Tasks/Task_004.py
+++ src/Tasks/Task_004.py
@@ -2,12 +2,12 @@
 # A = 1/2 * Base * Height
 
 
-Base = 10
-Height = 5
+Base = float(input("Enter Base of the Triangle: "))
+Height = float(input("Enter Height of the Triangle: "))
 
 Area = 1/2 * Base * Height
 print(Area)
 
 
 number = float(input("Enter a Number "))
-print("Yippie the Number is greater than 100" if number > 100 else "Sorry Number is less than 100")
+print("Yippie the Number is greater than 100" if number >= 100 else "Sorry Number is less than 100")
