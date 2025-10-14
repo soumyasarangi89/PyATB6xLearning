de4fe282fd18672a924fd62f34f570f2bf259199
soumyasarangi89 <112792910+soumyasarangi89@users.noreply.github.com>
2025-10-14 23:11:25 +0530

    14th Oct 2025 Task

--- src/Tasks/Task_003.py
+++ src/Tasks/Task_003.py
@@ -5,6 +5,26 @@ r = float(input("Enter the radius: "))
 area = π * pow(r,2)
 area51 = math.pi * pow(r,2)
 
+
+# i/p - r - float
+# o/p  -> string formatted output of area.
+
+
+# Logic Building Formula
+# || Step 1 ||
+# Figure out the inputs and output
+# input -> r ->  data type -> float
+# pi = 3.14
+# power -> pow or ** -> any
+# o/p -> String -> float - area , print area
+
+# || Step 2 ||
+# rough logic =  area = 3.14 * pow(r,2)
+
+
+# || Step 3 ||
+
+
 print("Area of the Circle =",area)
 
 # Formatting
---
+++ src/Tasks/Task_004.py
@@ -0,0 +1,13 @@
+# Area of a triangle
+# A = 1/2 * Base * Height
+
+
+Base = 10
+Height = 5
+
+Area = 1/2 * Base * Height
+print(Area)
+
+
+number = float(input("Enter a Number "))
+print("Yippie the Number is greater than 100" if number > 100 else "Sorry Number is less than 100")
