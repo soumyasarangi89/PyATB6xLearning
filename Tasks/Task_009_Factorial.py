4f0b6e9ad5d2562cdf0a63839d2c95f3b51728f6
soumyasarangi89 <112792910+soumyasarangi89@users.noreply.github.com>
2025-10-30 08:00:07 +0530

    28th Oct 2025

--- Tasks/Task_009_Factorial.py
+++ Tasks/Task_009_Factorial.py
@@ -1,19 +1,14 @@
-"""
-Given a Number a number you need to calculate the factorial of that number
-n = 5
-Fact = 5×4×3*2*1 = 120
-Fact = 0 → 1,
-"""
-
-# Take the input from user
-n = int(input("Enter a number: "))
+num = int(input("Enter a number which fact you want to get!"))
+# 5
 fact = 1
 
-if n < 0:
-    print(f"Factorial does not exist for negative numbers.")
-elif n == 0:
-    print(f"The factorial of {n} is always 1")
+if num < 0:
+    print("Fact is not defined!")
+
+if num == 0:
+    print("FACT = ", fact)
 else:
-    for i in range(1,n+1):
+    for i in range(1, num + 1):
         fact = fact * i
-    print(f"The factorial of {n} is {fact}")
+
+print("Factorial of : ", fact)
